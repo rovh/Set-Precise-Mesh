dc827b78961f50a4e659f6db6f66626e38b734ad
rovh <56371012+rovh@users.noreply.github.com>
2020-07-06 21:57:52 +0300

    Code

--- Draw.py
+++ Draw.py
@@ -11,7 +11,7 @@ def draw_callback_px(self, context):
     font_id = 0  # XXX, need to find out how best to get this.
 
     # draw some text
-    blf.position(font_id, 60, 30, 0)
+    blf.position(font_id, 15, 30, 0)
     blf.size(font_id, 20, 72)
     blf.draw(font_id, "Hello Word " + str(len(self.mouse_path)))
 
@@ -29,44 +29,44 @@ def draw_callback_px(self, context):
     # bgl.glDisable(bgl.GL_BLEND)
 
 
-
-
-class ModalTimerOperator(bpy.types.Operator):
-    """Operator which runs its self from a timer"""
-    bl_idname = "wm.modal_timer_operator"
-    bl_label = "Modal Timer Operator"
-
-    _timer = None
+class ModalDrawOperator(bpy.types.Operator):
+    """Draw a line with the mouse"""
+    bl_idname = "view3d.modal_operator"
+    bl_label = "Simple Modal View3D Operator"
 
     def modal(self, context, event):
-        if event.type in {'RIGHTMOUSE', 'ESC'}:
-            wm = context.window_manager
-            wm.event_timer_remove(self._timer)
+        context.area.tag_redraw()
+
+        if event.type == 'MOUSEMOVE':
+            self.mouse_path.append((event.mouse_region_x, event.mouse_region_y))
+
+        elif event.type == 'LEFTMOUSE':
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
+            return {'FINISHED'}
+
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:
+            bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
             return {'CANCELLED'}
 
-        if event.type == 'TIMER':
-
-            
-
-        return {'PASS_THROUGH'}
-
-    def execute(self, context):
-        wm = context.window_manager
-        self._timer = wm.event_timer_add(0.1, window=context.window)
-        wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
-
 
     def invoke(self, context, event):
         if context.area.type == 'VIEW_3D':
+            # the arguments we pass the the callback
             args = (self, context)
+            # Add the region OpenGL drawing callback
+            # draw in view space with 'POST_VIEW' and 'PRE_VIEW'
             self._handle = bpy.types.SpaceView3D.draw_handler_add(draw_callback_px, args, 'WINDOW', 'POST_PIXEL')
+
+            self.mouse_path = []
+
             context.window_manager.modal_handler_add(self)
             return {'RUNNING_MODAL'}
         else:
             self.report({'WARNING'}, "View3D not found, cannot run operator")
             return {'CANCELLED'}
-        
+
+
 
 if __name__ == "__main__":
     register()
